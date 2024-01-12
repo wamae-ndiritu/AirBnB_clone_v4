fbae18b8c02bde498dbfa1d4a4a9e7c6be72b674
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-01-12 09:58:03 +0000

    Handle selecting amenities

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,33 @@
+console.log("Document loading...");
+$(document).ready(function() {
+	console.log("Document ready...");
+	let selectedAmenities = {};
+	
+	// Listen for changes on each checkbox input
+	$('input[type="checkbox"]').chnage(function() {
+		const amenity_id = $(this).data('id');
+		const amenity_name = $(this).data('name');
+
+		if ($(this).is(':checked')) {
+			// checkbox is checked, we store amenity ID in the selectedAmenities
+			selectedAmenities[amenity_id] = amenity_name;
+		} else {
+			// checkbox not checked, we remove the amenity ID from the dictionary
+			delete selectedAmenities[amenity_id];
+		}
+		console.log(selectedAmenities);
+
+		// Update the h4 tag inside the div Amenities with the list of amenities checked
+		updateSelectedAmenitiesList();
+	});
+
+	// Function to update h4 in div Amenities
+	function updateSelectedAmenitiesList() {
+		let amenitiesH4 = $('.amenities h4');
+		const amenityNames = Object.values(selectedAmenities).join(', ');
+
+		// Pupolate the h4 with the selected amenities
+		amenitiesH4.text(amenityNames);
+		}
+	}
+});
