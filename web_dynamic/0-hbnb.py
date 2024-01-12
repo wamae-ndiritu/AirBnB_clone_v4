8f98f67405118fe954c03330ee3731a8e8e5b67a
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-01-12 08:41:40 +0000

    Implement cache asset busting Allows clients to get the latest version of static resources since assets are frequently modified

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """ Starts a Flask Web Application """
-from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
 from os import environ
 from flask import Flask, render_template
+from models import storage
+import uuid
+
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
@@ -18,28 +14,13 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/0-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-
-    return render_template('100-hbnb.html',
-                           states=st_ct,
-                           amenities=amenities,
-                           places=places)
-
+    cache_id = str(uuid.uuid4())
+    
+    return render_template('0-hbnb.html', cache_id=cache_id)
 
 if __name__ == "__main__":
-    """ Main Function """
+    """Start the app"""
     app.run(host='0.0.0.0', port=5000)
